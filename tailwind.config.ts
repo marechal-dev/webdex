f0bb34862f08c17c9add771a3308dadc2f38a198
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-10-17 21:40:49 -0300

    feat: add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'valor-100': '#ea0600',
+      'valor-200': '#a70c08',
+      'valor-300': '#730300',
+      'valor-500': '#571412',
+      'valor-950': '#201a1a',
+      'mystic-50': '#3aa5c9',
+      'mystic-100': '#3A78C9',
+      'mystic-200': '#3B4CCA',
+      'mystic-300': '#4A528A',
+      'instinct-100': '#ffde00',
     },
   },
   plugins: [],
